8c29481a8b48c5062799e8042288d192715c9c7f
Kevin <103201999+StringArrow@users.noreply.github.com>
2022-12-03 14:21:35 +0100

    Kreis bearbeitet / Kugel

--- WFA_GeomFigur/class/CKugel.cs
+++ WFA_GeomFigur/class/CKugel.cs
@@ -43,13 +43,13 @@ namespace WFA_GeomFigur
         {
             //Lord forgive me for what I am about to code ✝
             const double ABDUNKELNMULTIPLIKATOR = 0.98;
-            double LIGHTOFFSET = 1f / 3f * coordinates.Width;
-            const int ANZAHLGRADIENTEN = 100;
+            const int ANZAHLGRADIENTEN = 80;
 
             //Koordinaten des Gradianten-Teils
             Rectangle circleRectangle = coordinates;
 
             //Ausgangsfarbe vordunkeln, um nach der Schleife die Originalfarbe zu erhalten
+            //Speichern im double, wegen erhöhter Farbpräzision
             double r = solidBrush.Color.R / ANZAHLGRADIENTEN;
             double g = solidBrush.Color.G / ANZAHLGRADIENTEN;
             double b = solidBrush.Color.B / ANZAHLGRADIENTEN;
@@ -58,12 +58,18 @@ namespace WFA_GeomFigur
             double gIncrement = g;
             double bIncrement = b;
 
+            //Koordinaten
+            double x = circleRectangle.X;
+            double y = circleRectangle.Y;
+
             //Lichteinfall vortäuschen durch Helligkeit-Gradienten
             for (int i = 0; i < ANZAHLGRADIENTEN; i++)
             {
                 //Koordinaten des nächsten Kreises
-                circleRectangle.X = Convert.ToInt32(circleRectangle.X + LIGHTOFFSET);
-                circleRectangle.Y = Convert.ToInt32(circleRectangle.Y + LIGHTOFFSET);
+                x = x + circleRectangle.Width / 110;
+                y = y + circleRectangle.Height / 110;
+                circleRectangle.X = Convert.ToInt32(x);
+                circleRectangle.Y = Convert.ToInt32(y);
                 circleRectangle.Width = Convert.ToInt32(circleRectangle.Width * ABDUNKELNMULTIPLIKATOR);
                 circleRectangle.Height = Convert.ToInt32(circleRectangle.Height * ABDUNKELNMULTIPLIKATOR);
 
